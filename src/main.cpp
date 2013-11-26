a3017e38899f2b0369daa9c4a92624b32e63ea31
Štěpán Heller <heller.stepan@gmail.com>
2013-11-26 11:18:50 +0100

    OpenMP solution with active waiting

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <stack>
 #include <mpi.h>
+#include <omp.h>
 #include <cstdlib>
 
 #define JOB_REQUEST 0
@@ -29,7 +30,7 @@ typedef struct {
 } state;
 
 /* Globals */
-int cities = 5;
+/*int cities = 5;
 int distances[5][5] = {
 		{0,2,3,5,4},
 		{2,0,6,3,7},
@@ -37,7 +38,21 @@ int distances[5][5] = {
 		{5,3,1,0,2},
 		{4,7,4,2,0}};
 int bestRoute[6] = {0,0,0,0,0,0};
-state start = {0,{-1,-1,-1,-1,-1},0};
+state start = {0,{-1,-1,-1,-1,-1},0};*/
+
+int cities = 8;
+int distances[8][8] = {
+		{0,1,2,3,4,5,6,7},
+		{1,0,6,3,7,8,9,5},
+		{2,6,0,1,4,7,4,5},
+		{3,3,1,0,2,8,2,1},
+		{4,7,4,2,0,6,4,2},
+		{5,8,7,8,6,0,7,1},
+		{6,9,4,2,4,7,0,9},
+		{7,5,5,1,2,1,9,1}};
+int bestRoute[9] = {0,0,0,0,0,0,0,0,0};
+state start = {0,{-1,-1,-1,-1,-1,-1,-1,-1},0};
+
 /*int cities = 10;
 int distances[10][10] = {
 	{0,2,2,2,2,2,2,2,2,2},
@@ -57,10 +72,17 @@ int bestLength = 1<<16;
 /* Stack of states to process */
 stack <state> jobs;
 
+int stop = 0;
+
 /* Save the state to the stack to process */
 void toQueue(state inputState) {
 
-	jobs.push(inputState);
+	#pragma omp critical 
+	{
+		jobs.push(inputState);
+		stop = 0;
+		#pragma omp flush(stop)
+	}
 }
 
 /* Calculate distance between two states */
@@ -118,8 +140,8 @@ void printBest() {
 /* Find the successors of given state */
 void findSuccessors(state parentState) {
 
-	state currentState = {0,{-1,-1,-1,-1,-1},0};
-	jobs.pop();
+	state currentState = {0,{-1,-1,-1,-1,-1,-1,-1,-1},0};
+	//jobs.pop();
 	int flag = 0; // Indication that successor was find
 
 	for(int i = (cities -1); i >= 0 ; i--) {
@@ -136,10 +158,13 @@ void findSuccessors(state parentState) {
 	/* All states are in the path. Calculate the distance to the start */
 	if (flag == 0) {
 		if ((parentState.val + distances[parentState.id][start.id]) < bestLength) {
-			bestLength = parentState.val + distances[parentState.id][start.id];
-			memcpy(bestRoute, parentState.path, (cities-1)*sizeof(int));
-			/* Add next-to-last state to the path */
-			bestRoute[sizeof(bestRoute)/sizeof(int) - 2] = parentState.id;
+			#pragma omp critical 
+			{
+				bestLength = parentState.val + distances[parentState.id][start.id];
+				memcpy(bestRoute, parentState.path, (cities-1)*sizeof(int));
+				/* Add next-to-last state to the path */
+				bestRoute[sizeof(bestRoute)/sizeof(int) - 2] = parentState.id;
+			}
 			//printBest();
 		}
 	}
@@ -157,6 +182,18 @@ void myBcast(int root, int tag) {
 	}
 }
 
+bool finished(int waiting[]) {
+	int sum = 0;
+
+	for (int i = 0; i < 4; i++) {
+		sum += waiting[i];
+	}
+
+	if(sum == 4) return true;
+
+	return false;
+}
+
 
 int main(int argc, char *argv[]) {
 	int rank, size, data;
@@ -167,6 +204,9 @@ int main(int argc, char *argv[]) {
 	/* Find the rank of the process and number of processes */
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	/* Set number of threads to 4 */
+	omp_set_num_threads(4);
 
 	if (rank == 0)		
 	{
@@ -214,7 +254,10 @@ int main(int argc, char *argv[]) {
 
 		while(1) {
 			int city_id;
+			int flag = 0;
+			int waiting[4] = {0,0,0,0};
 			MPI_Status status;
+			state curr_state;
 
 			/* Ask for a job */
 			MPI_Send(0, 0, MPI_INT, MASTER_PROCESS, JOB_REQUEST, MPI_COMM_WORLD);
@@ -227,18 +270,41 @@ int main(int argc, char *argv[]) {
 			if (status.MPI_TAG == SEND_RESULTS) break;
 
 			/* Reconstruct the state */
-			state state_tbd = {city_id, {0,-1,-1,-1,-1}, calcVal(state_tbd, start)};
+			state state_tbd = {city_id, {0,-1,-1,-1,-1,-1,-1,-1}, calcVal(state_tbd, start)};
 			jobs.push(state_tbd);
 
-			/* Count the path */
-			while(jobs.size() > 0) {
-				//cout << "Processing state: " << jobs.top().id << endl;
-				findSuccessors(jobs.top());
-			}
+			/* Parallel section */
+			#pragma omp parallel shared(jobs, stop, waiting) private(curr_state, flag)
+			{
+				/* Count the path */
 
+				while(!finished(waiting)) {
+					/* Indicate that you are waiting for work */
+					waiting[omp_get_thread_num()] = 1;
+
+					while(!stop) {
+						flag = 0;
+
+						#pragma omp critical 
+						{
+							if (jobs.size() > 0 && !stop) {
+								curr_state = jobs.top();
+								jobs.pop();
+								flag = 1;
+								waiting[omp_get_thread_num()] = 0;
+							} else {
+								stop = 1;
+								#pragma omp flush(stop)
+							}
+							
+						}
+						if(flag) findSuccessors(curr_state);
+					}
+				}
+			}
 		}
 
-		/* Send best case to the master */
+		/* Send best case to master */
 		position = 0;
 		MPI_Pack(bestRoute, (cities + 1)*sizeof(int), MPI_INT, buffer, LENGTH, &position, MPI_COMM_WORLD);
 		MPI_Pack(&bestLength, 1, MPI_INT, buffer, LENGTH, &position, MPI_COMM_WORLD);
